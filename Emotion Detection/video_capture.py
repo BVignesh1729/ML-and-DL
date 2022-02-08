12effecf82cdbcc9b4e1feaa4df22d54674e611d
B Vignesh <63103172+BVignesh1729@users.noreply.github.com>
2022-02-08 22:58:48 +0530

    Add files via upload

--- Emotion Detection/video_capture.py
+++ Emotion Detection/video_capture.py
@@ -1,61 +1,132 @@
-# importing needed modules-----------------------
-import cv2, torch
+import os
+import cv2
+import torch
 import numpy as np
-from model import *
-
-# defining important funtions---------------------
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# function to turn photos to tensor
-def img2tensor(x):
-    transform = transforms.Compose(
-            [transforms.ToPILImage(),
-            transforms.Grayscale(num_output_channels=1),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5), (0.5))])
-    return transform(x)
-
-# the model for predicting
-model = model(1, 7)
-softmax = torch.nn.Softmax(dim=1)
-model.load_state_dict(torch.load('Resnet.pth', map_location=get_default_device()))
-
-def predict(x):
-    out = model(img2tensor(img)[None])
-    scaled = softmax(out)
-    prob = torch.max(scaled).item()
-    label = classes[torch.argmax(scaled).item()]
-    return {'label': label, 'probability': prob}
-
-if __name__ == "__main__":
-    vid = cv2.VideoCapture(0)
-
-    while vid.isOpened():
-        _, frame = vid.read()
-
-        # takes in a gray coloured filter of the frame
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # initializing the haarcascade face detector
-        faces = face_cascade.detectMultiScale(frame)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-
-            # takes the region of interest of the face only in gray
-            roi_gray = gray[y:y+h, x:x+h]
-            resized = cv2.resize(roi_gray, (48, 48))    # resizes to 48x48 sized image
-
-            # predict the mood
-            img = img2tensor(resized)
-            prediction = predict(img)
-
-            cv2.putText(frame, f"{prediction['label']}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0))
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as tt
 
 
-        cv2.imshow('video', frame)
-        if cv2.waitKey(1) == 27:
-            break
+face_classifier = cv2.CascadeClassifier("./models/haarcascade_frontalface_default.xml")
+model_state = torch.load("./models/emotion_detection_model_state.pth")
+class_labels = ["Angry", "Happy", "Neutral", "Sad", "Suprise"]
 
-    vid.release()
-    cv2.destroyAllWindows()
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ELU(inplace=True),
+    ]
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+    ]
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+
+        self.input = conv_block(in_channels, 64)
+
+        self.conv1 = conv_block(64, 64, pool=True)
+        self.res1 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
+        self.drop1 = nn.Dropout(0.5)
+
+        self.conv2 = conv_block(64, 64, pool=True)
+        self.res2 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
+        self.drop2 = nn.Dropout(0.5)
+
+        self.conv3 = conv_block(64, 64, pool=True)
+        self.res3 = nn.Sequential(conv_block(64, 32), conv_block(32, 64))
+        self.drop3 = nn.Dropout(0.5)
+
+        self.classifier = nn.Sequential(
+            nn.MaxPool2d(6), nn.Flatten(), nn.Linear(64, num_classes)
+        )
+
+    def forward(self, xb):
+        out = self.input(xb)
+
+        out = self.conv1(out)
+        out = self.res1(out) + out
+        out = self.drop1(out)
+
+        out = self.conv2(out)
+        out = self.res2(out) + out
+        out = self.drop2(out)
+
+        out = self.conv3(out)
+        out = self.res3(out) + out
+        out = self.drop3(out)
+
+        return self.classifier(out)
+
+
+model = ResNet(1, len(class_labels))
+model.load_state_dict(model_state)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Grab a single frame of video
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    labels = []
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray]) != 0:
+            roi = tt.functional.to_pil_image(roi_gray)
+            roi = tt.functional.to_grayscale(roi)
+            roi = tt.ToTensor()(roi).unsqueeze(0)
+
+            # make a prediction on the ROI
+            tensor = model(roi)
+            pred = torch.max(tensor, dim=1)[1].tolist()
+            label = class_labels[pred[0]]
+
+            label_position = (x, y)
+            cv2.putText(
+                frame,
+                label,
+                label_position,
+                cv2.FONT_HERSHEY_COMPLEX,
+                2,
+                (0, 255, 0),
+                3,
+            )
+        else:
+            cv2.putText(
+                frame,
+                "No Face Found",
+                (20, 60),
+                cv2.FONT_HERSHEY_COMPLEX,
+                2,
+                (0, 255, 0),
+                3,
+            )
+
+    cv2.imshow("Emotion Detector", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
